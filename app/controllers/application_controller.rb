f1fec02f056a2ade1c1ae9ce85dd16f6d375638a
kendric jones <kendric.jones@gmail.com>
2015-03-12 13:10:12 -0400

    Added Pundit policies to controllers and views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,18 +1,15 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
+    include Pundit
   protect_from_forgery with: :exception
-     before_action :configure_permitted_parameters, if: :devise_controller?
-     skip_before_action :flash_attack, only: [:index, :new]
-   protected
- 
-   def configure_permitted_parameters
-     devise_parameter_sanitizer.for(:sign_up) << :name
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+     rescue_from Pundit::NotAuthorizedError do |exception|
+     redirect_to root_url, alert: exception.message
    end
 
-private
-  def flash_attack
-    flash[:notice] = "Thank you for visiting!"
-  end
+  protected
 
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :name
+  end
 end
