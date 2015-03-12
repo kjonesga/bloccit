44360ee9b5a8a2cf0ad99b4093000a5aa9c4cfee
kendric jones <kendric.jones@gmail.com>
2015-03-11 21:56:49 -0400

    Complete Associations assignment

--- Gemfile
+++ Gemfile
@@ -27,6 +27,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'bootstrap-sass'
 gem 'devise'
 gem 'figaro'
+gem 'hirb'
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,10 @@
 class Post < ActiveRecord::Base
 	has_many :comments
 	belongs_to :user
-	default_scope { order('created_at DESC') }
+	#default_scope { order('created_at DESC') }
+	scope :ordered_by_title, -> { order(title: :desc) }
+	scope :ordered_by_reverse_created_at, -> {order(created_at: :desc)}
 end
+
+
+	#scope :ordered_by_field, -> (field) { order("#{field} DESC") }
